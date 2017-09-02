de66607ca2f281fcd1fbd7727b6ffaba3d923081
davidfisher24 <you@example.com>
2017-09-02 18:14:06 +0200

    Added model functions to get full cima info

--- app/Enlace.php
+++ app/Enlace.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Enlace extends Model
 {
-    //
+    /**
+     * Relationship - Each enlace belongs to a single vertiente 
+     *
+     * @collection cimeros
+     */
+    public function vertiente()
+    {
+        return $this->belongsTo('App\Vertiente');
+    }
 }
--- app/Vertiente.php
+++ app/Vertiente.php
@@ -15,4 +15,14 @@ class Vertiente extends Model
     {
         return $this->belongsTo('App\Cima');
     }
+
+    /**
+     * Relationship - Vertiente has various enlaces logros
+     *
+     * @collection enlaces
+     */
+    public function enlaces()
+    {
+        return $this->hasMany('App\Enlace');
+    }
 }
