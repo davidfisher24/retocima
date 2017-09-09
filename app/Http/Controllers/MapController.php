93efcea4e3ea01d37ea31459a654a33cc27ac005
David <davidfishe24@gmail.com>
2017-09-09 12:33:36 +0200

    Added simple information window to map

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -15,7 +15,16 @@ class MapController extends Controller
         Mapper::map(40.416775, -3.703790, ['zoom' => 6, 'marker' => []]);
         foreach($cimas as $cima) {
         	$clickEvent = 'mouseClickCima(' . $cima . ')';
-        	Mapper::marker($cima->longitude, $cima->latitude, [
+        	/*Mapper::marker($cima->longitude, $cima->latitude, [
+        		'eventClick' => $clickEvent,
+        	]);*/
+
+            $contentString = '<p class="panel-heading"><strong>'. $cima->codigo . ' ' . $cima->nombre . '</strong></p>';
+            $contentString .= '<a href="/detallescima/' . $cima->id . '" target="_BLANK">Mas Detalles</a>';
+        	Mapper::informationWindow($cima->longitude, $cima->latitude, $contentString, [
+        		'open' => false, 
+        		'maxWidth'=> 300, 
+        		'markers' => ['title' => 'Title'],
         		'eventClick' => $clickEvent,
         	]);
         }
--- resources/views/publicarea/mapa.blade.php
+++ resources/views/publicarea/mapa.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Estadistica</div>
+                <div class="panel-heading">Mapa de Cimas</div>
 
                 <div class="panel-body">
                     <div class="col-md-8">
