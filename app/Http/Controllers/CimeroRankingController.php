57b7a658dec4d53da87706bb2c9cafd0778943b7
davidfisher24 <davidfisher24@gmail.com>
2017-10-11 13:33:42 +0200

    Testing graphics

--- app/Http/Controllers/CimeroRankingController.php
+++ app/Http/Controllers/CimeroRankingController.php
@@ -8,7 +8,9 @@ use App\Services\CimeroLogroService;
 use App\Services\AreaCompletionService;
 
 use App\Cimero;
+use App\Communidad;
 
+use Khill\Lavacharts\Lavacharts;
 
 class CimeroRankingController extends Controller
 {
@@ -69,7 +71,58 @@ class CimeroRankingController extends Controller
         $completedProvinces = $this->areaCompletionService->getCimerosCompletedProvinces($id);
         $completedCommunidads = $this->areaCompletionService->getCimerosCompletedCommunidads($id);
 
-        return view('publicarea.cimeropublicdetails',compact('cimero','logros','completions','completedProvinces','completedCommunidads'));
+
+        // CHART TO PUT IN A SERVICE
+        $lava = new Lavacharts;
+
+        $logrosChart = $lava->DataTable();
+
+        $logrosChart->addStringColumn('Communidad')
+                ->addNumberColumn('Logros');
+
+        // DATA
+        $logrosCountChart2 = array();
+        foreach($logros as $comm) {
+            $logrosChart->addRow([$comm->first()->first()->communidad->nombre,count($comm->first())]);
+            $logrosCountChart2[$comm->first()->first()->communidad->id] = count($comm->first());
+        }
+        // END DATA
+
+        $lava->PieChart('chart_1', $logrosChart, [
+            'title'  => 'Logros Por Provincia',
+            'is3D'   => true,
+            'legend' => array( 'position' => 'left'),
+        ]);
+        // END CHART BLCOCK -> HAVE TO ADD AS A VIEW PARARM
+
+        // SECOND CHART
+
+        $percentCompleteChart = $lava->DataTable();
+
+        $percentCompleteChart->addStringColumn('Communidad')
+             ->addNumberColumn('Mis Logros')
+             ->addNumberColumn('Total Cimas');
+
+        // DATA
+        foreach(Communidad::all() as $communidad) {
+            $percentCompleteChart->addRow([
+                $communidad->nombre, 
+                array_key_exists($communidad->id,$logrosCountChart2) ? $logrosCountChart2[$communidad->id] : 0, 
+                $communidad->cimas()->count()
+            ]);
+        }
+        // END DATA
+        $lava->ColumnChart('chart_2', $percentCompleteChart, [
+            'title' => 'Mis Logros comparadao con total',
+            'titleTextStyle' => [
+                'color'    => '#eb6b2c',
+                'fontSize' => 14
+            ]
+        ]);
+        // END SECOND CHART BLCOCK -> HAVE TO ADD AS A VIEW PARARM
+        
+
+        return view('publicarea.cimeropublicdetails',compact('cimero','logros','completions','completedProvinces','completedCommunidads','lava'));
 
     }
 
--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Cornford\Googlmapper\MapperServiceProvider::class,
+        Khill\Lavacharts\Laravel\LavachartsServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -210,6 +211,7 @@ return [
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
+        'Lava' => Khill\Lavacharts\Laravel\LavachartsFacade::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Mapper' => Cornford\Googlmapper\Facades\MapperFacade::class,
